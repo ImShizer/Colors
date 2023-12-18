f4ad64b91b86dfa21abe7a152fb3008baca2ef31
ImShizer <68175695+ImShizer@users.noreply.github.com>
2023-12-18 04:48:10 +0200

    End of Speedrun

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Colors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            KeyDown += MainWindow_KeyDown;
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                switch (e.Key)
+                {
+                    case Key.R:
+                        ChangeButtonColor(System.Windows.Media.Colors.Red);
+                        break;
+                    case Key.G:
+                        ChangeButtonColor(System.Windows.Media.Colors.Green);
+                        break;
+                    case Key.B:
+                        ChangeButtonColor(System.Windows.Media.Colors.Blue);
+                        break;
+                }
+            }
+        }
+
+        private void ChangeButtonColor(Color color)
+        {
+            myButton.Background = new SolidColorBrush(color);
+        }
+    }
+}
+
